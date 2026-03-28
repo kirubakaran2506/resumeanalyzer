0b968dfc7f5d71a1d2e92a3d4692943eea47c0db
kirubakaran2506 <kirubakaran250605@gmail.com>
2026-03-28 18:55:23 +0530

    app.py

--- app.py
+++ app.py
@@ -226,7 +226,8 @@ def analyze():
 
 
 # ─── RUN ──────────────────────────────────────────────
+import os
 
 if __name__ == "__main__":
-   #app.run(debug=True)
-   app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
